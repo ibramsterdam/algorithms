4d9f230c3a0193f15aaed9f4630eb07fd447a6ad
Bram Janssen <ibramsterdam@hotmail.com>
2022-11-26 23:15:25 +0100

    Crystal Balls algorithm

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpAmount;
+
+    // Jump the jumpAmount until first ball breaks
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) break;
+    }
+
+    // Set i back to one jumpAmount
+    i -= jumpAmount;
+
+    // Walk the last jumpAmount
+    for (let j = 0; j < jumpAmount && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
