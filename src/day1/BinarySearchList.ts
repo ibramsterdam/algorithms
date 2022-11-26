fa9da6d2223bb0988fe981377011039d35a2b70c
Bram Janssen <ibramsterdam@hotmail.com>
2022-11-26 22:38:44 +0100

    Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const midpoint = Math.floor(low + (high - low) / 2);
+        const value = haystack[midpoint];
+
+        if (needle === value) return true;
+        if (needle > value) low = midpoint + 1;
+        if (needle < value) high = midpoint;
+    } while (low < high);
+
+    return false;
 }
