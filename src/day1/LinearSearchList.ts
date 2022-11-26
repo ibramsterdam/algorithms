03f0ea6e966fd07c6732953c8d2d925eb1e14e6e
Bram Janssen <ibramsterdam@hotmail.com>
2022-11-26 22:04:29 +0100

    add: LinearSearch

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,9 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (const entity of haystack) {
+        if (entity === needle) return true;
+    }
+    return false;
 }
