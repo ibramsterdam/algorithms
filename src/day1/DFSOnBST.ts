ab104d2b5eee3087d3d9ab6a148aefca31295506
Bram Janssen <ibramsterdam@hotmail.com>
2023-06-30 22:45:46 +0200

    feat: Dept first search on BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,9 +1,13 @@
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+    if (curr === null) return false;
+
+    if (curr.value === needle) return true;
+
+    if (curr.value < needle) {
+        return search(curr.right, needle);
+    }
+    return search(curr.left, needle);
+}
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-    if (head === null) return false;
-    if (head.value === needle) return true;
-
-    if (needle < head.value && head.left) dfs(head.left, needle);
-    if (needle > head.value && head.right) dfs(head.right, needle);
-
-    return false;
+    return search(head, needle);
 }
