a870a07db3be38d112e62beeea977758b2b07e72
Bram <ibramsterdam@hotmail.com>
2023-06-30 20:32:55 +0200

    progress

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,9 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    if (head === null) return false;
+    if (head.value === needle) return true;
 
+    if (needle < head.value && head.left) dfs(head.left, needle);
+    if (needle > head.value && head.right) dfs(head.right, needle);
+
+    return false;
 }
